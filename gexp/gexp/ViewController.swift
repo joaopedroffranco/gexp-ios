e7969e6694ab8e770fe4cd5d2579fd17e0843e7d
joaopedroffranco <joao.franco@dextra-sw.com>
2020-12-14 11:22:23 -0300

    chore: creating project

---
+++ gexp/gexp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gexp
+//
+//  Created by Joao Pedro Fabiano Franco on 14/12/20.
+//  Copyright © 2020 Dextra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ gexpTests/gexpTests.swift
@@ -0,0 +1,34 @@
+//
+//  gexpTests.swift
+//  gexpTests
+//
+//  Created by Joao Pedro Fabiano Franco on 14/12/20.
+//  Copyright © 2020 Dextra. All rights reserved.
+//
+
+import XCTest
+@testable import gexp
+
+class gexpTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
